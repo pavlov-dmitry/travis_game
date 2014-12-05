bf1b3a8cfaecb2d9c1e03a62a9fdf03866a7a0a9
pavlov-dmitry <voidwalker@mail.ru>
2014-12-05 05:38:41 -0800

    Добавил скрипт сборки libexif

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-travis_game
+travis_game [![Build Status](https://travis-ci.org/pavlov-dmitry/travis_game.png?branch=master)](https://travis-ci.org/pavlov-dmitry/travis_game)
 ===========
 
 Репозиторий на котором я хочу изучить работу с Трэвисом
--- build_libexif.sh
+++ build_libexif.sh
@@ -1,1 +1,5 @@
-echo "hello Travis!"
+#!/bin/sh
+set -ex
+curl -L http://downloads.sourceforge.net/project/libexif/libexif/0.6.21/libexif-0.6.21.zip > libexif.zip
+unzip libexif.zip -d libexif
+cd libexif/libexif-0.6.21 && ./configure --prefix=/usr --enable-shared && make && sudo make install
